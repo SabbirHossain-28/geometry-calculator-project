95e416b0c0f5ea46a71f2bc7b0007b9f41972897
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-02-13 21:59:15 +0600

    complete the triangle area calculation functinality with js

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ function takeInputByInputField(inputValue){
     const numberTypeInput=parseFloat(valueFromInputField);
     return(numberTypeInput)
 }
+
+function calculatedArea(areaId,area){
+    const getarea=document.getElementById(areaId,area);
+    getarea.innerText=area;
+    return(getarea)
+}
 function inputValue(){
-    const getInputValue=takeInputByInputField('input-field-base');
-    console.log(getInputValue);
+    const getBaseInputValue=takeInputByInputField('input-field-base');
+    const getHeightInputValue=takeInputByInputField('input-field-height');
+    const areaCalculated=0.5*getBaseInputValue*getHeightInputValue;
+    const areaValue=calculatedArea('area-of-triangle',areaCalculated)
 }
