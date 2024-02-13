ed218abdf26ff148fa9f80a3ac242b56059dfba0
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-02-13 22:38:09 +0600

    create rectangle area calculation card with area calculation funcitionalities

--- script.js
+++ script.js
@@ -10,9 +10,15 @@ function calculatedArea(areaId,area){
     getarea.innerText=area;
     return(getarea)
 }
-function inputValue(){
+function inputValue1(){
     const getBaseInputValue=takeInputByInputField('input-field-base');
     const getHeightInputValue=takeInputByInputField('input-field-height');
     const areaCalculated=0.5*getBaseInputValue*getHeightInputValue;
     const areaValue=calculatedArea('area-of-triangle',areaCalculated)
+}
+function inputValue2(){
+    const getBaseInputValue=takeInputByInputField('input-field-width');
+    const getHeightInputValue=takeInputByInputField('input-field-length');
+    const areaCalculated=getBaseInputValue*getHeightInputValue;
+    const areaValue=calculatedArea('area-of-rectangle',areaCalculated)
 }
